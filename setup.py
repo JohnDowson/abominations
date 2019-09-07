42ba319c416b350433799e2ed8b3c9547d16c272
Ivan Chinenov <ichinenov@gmail.com>
2019-09-07 20:16:52 +0300

    Initial commit II: Attack of typos

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='abominations',
       version='0.1',
       description='Ruby-like iteration',
-      url='thhps://github.com/JohnDowson/abominations',
+      url='https://github.com/JohnDowson/abominations',
       author='JohnDowson',
       author_email='ichinenov@gmail.com',
       license='GPL',
-      packages=['ruby_like_iteration'],
+      packages=['abominations'],
       zip_safe=False)
