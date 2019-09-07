8438a2b4b6730b307bc8db10dceffcbb198decf1
Ivan Chinenov <ichinenov@gmail.com>
2019-09-07 19:57:35 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='abominations',
+      version='0.1',
+      description='Ruby-like iteration',
+      url='thhps://github.com/JohnDowson/abominations',
+      author='JohnDowson',
+      author_email='ichinenov@gmail.com',
+      license='GPL',
+      packages=['ruby_like_iteration'],
+      zip_safe=False)
